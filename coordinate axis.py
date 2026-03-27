28ff62dbe5a73090440219f71a7967865573042d
Yellowduckk112 <3911084455@qq.com>
2026-03-27 19:22:36 +0800

    starting to learn axis

---
+++ coordinate axis.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(-2, 4, 0.1)
+y = x**3+3*x
+
+plt.figure()
+plt.plot(x, y, linewidth=1.5)
+plt.xlabel("xxxx")
+plt.ylabel("yyyy")
+plt.xlim((-4, 5))
+plt.ylim((-20, 70))
+
+ticks = np.arange(-1, 4, 0.5)
+plt.xticks(ticks)
+
+plt.show()
--- figure.py
+++ figure.py
@@ -5,5 +5,9 @@ x = np.arange(1, 10, 0.1)
 y1 = x*4
 y2 = np.exp(x)
 
+plt.figure(figsize=(4, 8))
+plt.plot(x, y1, label="y=4x", color="red")
 
-print(y2)
+plt.figure(num=4, figsize=(4, 7))
+plt.plot(x, y2, label="e^x", color='black')
+plt.show()
